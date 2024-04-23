9dc3fdf93b44f8592124d861943191e1053cc4d3
aivaras23 <aivaraspal1@gmail.com>
2024-04-23 18:11:39 +0300

    initial commit

---
+++ src/app/triangle/triangle.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-triangle',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './triangle.component.html',
+  styleUrl: './triangle.component.css'
+})
+export class TriangleComponent {
+    public a:number|null = null;
+    public b:number|null = null;
+    public c:number|null = null;
+
+    public perimetroSuma:number|null = null;
+    public semiPeri:number|null = null;
+    public plotoSuma:number|null = null;
+    public errorMsg:string|null = null;
+
+    public calc(){
+      if(this.a != null && this.b != null && this.c != null) {
+        this.perimetroSuma = this.a + this.b + this.c;
+
+        this.semiPeri = (this.a + this.b + this.c) / 2; 
+
+        if(
+          this.a + this.b > this.c &&
+          this.a + this.c > this.b &&
+          this.b + this.c > this.a 
+        ) {
+           this.plotoSuma = Number(Math.sqrt(this.semiPeri * (this.semiPeri - this.a) * (this.semiPeri - this.b) * (this.semiPeri - this.c) ).toFixed(2));
+           this.errorMsg = null;
+        }
+        else {
+          this.errorMsg = 'Neteisingas trikampis';
+          this.perimetroSuma = null;
+          this.plotoSuma = null;
+        }
+
+
+      }
+    }
+}
